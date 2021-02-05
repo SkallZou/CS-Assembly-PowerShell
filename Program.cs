0cc93c36ce83e2afff829f7ec492b47bd5c193b7
Olivier LEUNG <olivierleung@hotmail.fr>
2021-02-05 10:39:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AssemblyLoader
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Byte[] bytes =
+                File.ReadAllBytes(
+                    "C:\\Users\\Root\\Desktop\\Programming\\C#\\Seatbelt-master\\Seatbelt-master\\Seatbelt\\bin\\Debug\\Seatbelt.exe");
+            RunAssembly(bytes, new [] {"-group=user"});
+            Console.WriteLine("Press any key to quit.");
+            Console.Read();
+        }
+
+        public static void RunAssembly(Byte[] assemblyBytes, string[] param)
+        {
+            Assembly assembly = Assembly.Load(assemblyBytes); //Load the assembly in memory
+            MethodInfo entryPoint = assembly.EntryPoint; //Find the entry point of the assembly
+            object[] parameters = new[] {param};
+            object execute = entryPoint.Invoke(null, parameters);
+        }
+    }
+}
